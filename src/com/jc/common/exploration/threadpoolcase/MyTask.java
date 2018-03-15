8c40467db0e416b697bb99fde0b597b36563c4d1
jichaogit <461709567@qq.com>
2018-03-15 17:30:49 +0800

    init

--- src/com/jc/common/exploration/threadpoolcase/MyTask.java
+++ src/com/jc/common/exploration/threadpoolcase/MyTask.java
@@ -1,12 +1,27 @@
 package com.jc.common.exploration.threadpoolcase;
 
 public class MyTask implements  Runnable {
-    @Override
-    public void run() {
+
+    public void run2() {
         synchronized (Test1.class){
             Test1.i = Test1.i+1;
             Test1.set.add(Test1.i);
             System.out.println("size-"+ Test1.set.size());
+        }
+        System.out.println("i-"+ Test1.i);
+
+
+
+    }
+
+    public void run() {
+        Test1.i = Test1.i+1;
+        Test1.set.add(Test1.i);
+        System.out.println("size-"+ Test1.set.size());
+        try {
+            Thread.sleep(500);
+        } catch (InterruptedException e) {
+            e.printStackTrace();
         }
         System.out.println("i-"+ Test1.i);
 
--- src/com/jc/common/exploration/threadpoolcase/Test1.java
+++ src/com/jc/common/exploration/threadpoolcase/Test1.java
@@ -8,31 +8,52 @@ import java.util.concurrent.TimeUnit;
 
 /**
  * Java线程池的分析和使用
+ *
+ * corePoolSize 线程池的基本线程数
+ * maximumPoolSize 线程池最大线程数
+ * keepAliveTime 工作线程空闲后保持活动的时间
+ * milliseconds 单位
+ * runnableTaskQueue 任务队列  LinkedBlockingQueue
+ * threadFactory 自定义创建线程工厂
+ * rejectedExecutionHandler 饱和策略
+ *              AbortPolicy－默认抛异常 queue超过100 java.util.concurrent.ThreadPoolExecutor$AbortPolicy.rejectedExecution(ThreadPoolExecutor.java:2047)
+ *              DiscardPolicy-不处理丢弃掉
+ *              DiscardOldestPolicy－丢弃队列里最近的一个任务，并执行但前任务
+ *              CallerRunsPolicy 只用调用者所在线程来运行任务
+ *              RejectedExecutionHandler 可以实现接口自定义策略
  */
 public class Test1 {
     public static int i=0;
 
     public static Set<Integer> set = new HashSet<Integer>();
     public static  void main(String[] args) throws InterruptedException {
-        LinkedBlockingQueue queue = new LinkedBlockingQueue();
-        ThreadPoolExecutor executor1 = new ThreadPoolExecutor(5,10,5*60, TimeUnit.SECONDS,queue);
+          LinkedBlockingQueue queue = new LinkedBlockingQueue(100);
+          ThreadPoolExecutor executor1 = new ThreadPoolExecutor(5,10,5*60, TimeUnit.SECONDS,queue);
+//
+//
+          MyThreadFactory threadFactory = new MyThreadFactory();
+//        ThreadPoolExecutor executor2 = new ThreadPoolExecutor(5,10,5*60, TimeUnit.SECONDS,queue,threadFactory);
+//
+          MyRejectedExecutionHandler rejectedExecutionHandler = new MyRejectedExecutionHandler();
+//        ThreadPoolExecutor executor3 = new ThreadPoolExecutor(5,10,5*60, TimeUnit.SECONDS,queue,rejectedExecutionHandler);
+//
+//
+//        ThreadPoolExecutor executor4 = new ThreadPoolExecutor(5,10,5*60, TimeUnit.SECONDS,queue,threadFactory,rejectedExecutionHandler);
 
-
-        MyThreadFactory threadFactory = new MyThreadFactory();
-        ThreadPoolExecutor executor2 = new ThreadPoolExecutor(5,10,5*60, TimeUnit.SECONDS,queue,threadFactory);
-
-        MyRejectedExecutionHandler rejectedExecutionHandler = new MyRejectedExecutionHandler();
-        ThreadPoolExecutor executor3 = new ThreadPoolExecutor(5,10,5*60, TimeUnit.SECONDS,queue,rejectedExecutionHandler);
-
-
-        ThreadPoolExecutor executor4 = new ThreadPoolExecutor(5,10,5*60, TimeUnit.SECONDS,queue,threadFactory,rejectedExecutionHandler);
+        // 饱和策略 丢弃
+        executor1.setRejectedExecutionHandler( new ThreadPoolExecutor.CallerRunsPolicy());
 
 
         for(int i=0;i<1000;i++){
             MyTask task = new MyTask();
             executor1.execute(task);
+            System.out.println("TaskCount="+executor1.getTaskCount()+"  completedTaskCount="+executor1.getCompletedTaskCount()+ " largestPoolSize="+executor1.getLargestPoolSize()+" getPoolSize="+ executor1.getPoolSize()+" getActiveCount="+executor1.getActiveCount() +" queue＝"+queue.size());
+
+
 
         }
+
+
 
 
         Thread.sleep(5000);
