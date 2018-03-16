bf4f359c7dd51bd9dde396e5c1d5b8ce03830a65
jichaogit <461709567@qq.com>
2018-03-16 08:51:37 +0800

    forkjoincase

---
+++ src/com/jc/common/exploration/forkjoincase/Test.java
@@ -0,0 +1,66 @@
+package com.jc.common.exploration.forkjoincase;
+
+import java.util.concurrent.ExecutionException;
+import java.util.concurrent.ForkJoinPool;
+import java.util.concurrent.Future;
+import java.util.concurrent.RecursiveTask;
+
+public class Test {
+    public static void main(String[] args){
+        ForkJoinPool forkJoinPool =new ForkJoinPool();
+        CountTask task =new CountTask(1, 9);
+
+        Future result = forkJoinPool.submit(task);
+        try {
+            System.out.println(result.get());
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        } catch (ExecutionException e) {
+            e.printStackTrace();
+        }
+
+
+    }
+}
+
+
+class CountTask extends RecursiveTask {
+
+    private static final int THRESHOLD = 2;
+    private int start;
+    private int end;
+    public  CountTask(int start,int end){
+        this.start = start;
+        this.end = end;
+    }
+
+    @Override
+    protected Integer compute() {
+        int sum = 0;
+        boolean canCompute = (end-start) <= THRESHOLD;
+        if(canCompute){
+            for(int i=start;i<=end;i++){
+                sum += i;
+            }
+
+        }else {
+            int middle = (start+end) / 2;
+            CountTask leftTask =new CountTask(start, middle);
+            CountTask rightTask =new CountTask(middle + 1,end);
+
+            //执行子任务
+            leftTask.fork();
+            rightTask.fork();
+
+            //等待子任务执行完，并得到其结果
+            int leftResult = (int)leftTask.join();
+            int rightResult = (int)rightTask.join();
+
+            //合并子任务
+            sum = leftResult  + rightResult;
+
+        }
+
+        return sum;
+    }
+}
